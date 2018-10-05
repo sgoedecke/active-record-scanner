589865947fd21cda2dc9aa894f6382685ddca5a3
Sean Goedecke <sgoedecke@zendesk.com>
2018-10-05 14:38:37 +1000

    handle empty args

--- scanner.rb
+++ scanner.rb
@@ -1,5 +1,11 @@
+#!/usr/bin/env ruby
+
 require_relative './src/active_record_scanner'
 
-results = ActiveRecordScanner.new(ARGV[0]).scan
-puts "\r\n"
-puts results.join("\n")
+if ARGV[0]
+  results = ActiveRecordScanner.new(ARGV[0]).scan
+  puts "\r\n"
+  puts results.join("\n")
+else
+  puts "Usage -- scanner.rb '/path/to/file.rb'"
+end
